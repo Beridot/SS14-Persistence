d057539e36f38247c22ba726d013414ed068b4c1
Michael Chessall <chessall007@gmail.com>
2025-12-09 06:13:39 -0700

    Cargo Request Multi Trade Station Support

--- Content.Server/Cargo/Components/TradeStationComponent.cs
+++ Content.Server/Cargo/Components/TradeStationComponent.cs
@@ -1,3 +1,7 @@
+using Content.Shared.Cargo.Prototypes;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
 namespace Content.Server.Cargo.Components;
 
 /// <summary>
@@ -6,5 +10,12 @@ namespace Content.Server.Cargo.Components;
 [RegisterComponent]
 public sealed partial class TradeStationComponent : Component
 {
+    [DataField, AutoNetworkedField]
+    public int UID = 0;
 
+    [DataField]
+    public List<ProtoId<CargoMarketPrototype>> Markets = new()
+    {
+        "market",
+    };
 }
